9f79fbd6e705d1c4aabe458cbb66bcbe0e8ece2e
miaan15 <miaanstarman@gmail.com>
2026-06-15 20:59:20 +0700

    dynamic array, hash map, assert

--- src/strat/assert.cpp
+++ src/strat/assert.cpp
@@ -1,3 +1,25 @@
 module;
 
 export module assert;
+
+import std;
+
+export void _assert(
+    bool statement,
+    const char *message,
+    const std::source_location location = std::source_location::current()) {
+
+#ifndef CACTUS_DISABLE_ASSERT
+    if (statement) return;
+
+    std::cerr << "\033[1m" << location.file_name() << "\033[0m:"
+              << location.line() << ":"
+              << location.column() << ": "
+              << "\033[31mASSERTION FAILED\033[0m: "
+              << message
+              << std::endl;
+
+    std::exit(1);
+#endif
+}
+
--- src/strat/dyn_array.cpp
+++ src/strat/dyn_array.cpp
@@ -11,9 +11,76 @@ struct DynArray {
     size_t len = 0;
     size_t cap = 0;
 
+    void destroy() {
+        if (data) std::free(data);
+        data = nullptr;
+        len = cap = 0;
+    }
 
+    void reserve(size_t new_cap) {
+        if (new_cap <= cap) return;
+        T* new_data = std::malloc(new_cap * sizeof(T));
+
+        if (data) {
+            std::memcpy(new_data, data, len * sizeof(T));
+            std::free(data);
+        }
+
+        data = new_data;
+        cap = new_cap;
+    }
+
+    void grow(size_t min_cap) {
+        size_t new_cap = cap < 4 ? 4 : (cap * 2);
+        if (new_cap < min_cap) new_cap = min_cap;
+        reserve(new_cap);
+    }
+
+    void append(const T& val) {
+        if (len == cap) grow(len + 1);
+        data[len] = val;
+        ++len;
+    }
+
+    void append_range(T *range_begin, size_t range_len) {
+        if (len + range_len >= cap) grow(len + range_len);
+        std::memcpy(data + len, range_begin, range_len * sizeof(T));
+        len += range_len;
+    }
+
+    void pop() {
+        if (len > 0) --len;
+    }
+
+    void set(size_t index, const T& val) {
+        if (index < len) data[index] = val;
+    }
+
+    [[nodiscard]] T get(size_t index) const {
+        if (index >= len) return T{};
+        return data[index];
+    }
+
+    [[nodiscard]] T *get_ptr(size_t index) {
+        if (index >= len) return nullptr;
+        return &data[index];
+    }
+    [[nodiscard]] const T *get_ptr(size_t index) const {
+        if (index >= len) return nullptr;
+        return &data[index];
+    }
+
+    [[nodiscard]] bool empty() const { return len == 0; }
+
+    void clear() { len = 0; }
+
+    using iterator = T *;
+    using const_iterator = const T *;
+
+    iterator begin()  { return data; }
+    iterator end() { return data + len; }
+    const_iterator begin() const { return data; }
+    const_iterator end() const { return data + len; }
+    const_iterator cbegin() const { return data; }
+    const_iterator cend() const { return data + len; }
 };
-
-export template <typename T>
-[[nodiscard]] DynArray<T> DynArray_make() noexcept {
-}
