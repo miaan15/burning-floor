08922f3ab359c9f266f4bea7cc23d6c1727c3a3e
miaan15 <miaanstarman@gmail.com>
2026-08-08 00:22:18 +0700

    player do

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "context.h"
 #include "draw.h"
 #include "draw_resource.h"
+#include "log.h"
+#include <math.h>
 #include <SDL3/SDL.h>
 #include <stdalign.h>
 #include <stdint.h>
@@ -35,13 +37,142 @@ u32 texture_new_help(const char *name) {
     return texture_new(dir, renderer, SDL_SCALEMODE_NEAREST);
 }
 
-Vec2 player_input = {0};
 Vec2 player_pos = {0};
 u32 player_sprite = 0;
 
-void update();
-void frame_update();
-void render_update();
+Vec2 player_input = {0};
+Vec2 player_atk_input = {0};
+bool player_dash_input = false;
+
+const float player_move_speed = 1;
+const float player_atk_cd = 1;
+const float player_atk_dur = .3;
+const float player_dash_speed = 3;
+const float player_dash_cd = 1;
+const float player_dash_dur = .05;
+
+Vec2 player_move_dir = {0};
+Vec2 player_move_dir_nozero = {0};
+
+bool player_atk_able = false;
+bool player_atking = false;
+bool player_just_atk = false;
+char player_atk_dir = 0; // RLUD
+float player_atk_timest = -1000;
+
+bool player_dash_able = false;
+bool player_dashing = false;
+bool player_just_dash = false;
+Vec2 player_dash_dir = {0};
+float player_dash_timest = -1000;
+
+void setup() {
+    arena_init(&global_ar, 100ull << 10 << 10); // 100mB
+
+    { // input
+    int numkeys;
+    SDL_GetKeyboardState(&numkeys);
+    last_keyb_state = arena_alloc(&global_ar, numkeys, 1);
+    }
+
+    // Tex
+    texture_init(128);
+    u32 tex = texture_new_help("img_player");
+
+    // Sprite
+    sprite_init(1024);
+    SDL_FRect srect = {0, 0, 20, 20};
+    player_sprite = sprite_new(tex, &srect);
+
+    // Draw
+    draw_init(1024);
+}
+
+void input_update() {
+    player_input = (Vec2){0};
+    if (keyb_state[SDL_SCANCODE_D]) player_input.x += 1;
+    if (keyb_state[SDL_SCANCODE_A]) player_input.x -= 1;
+    if (keyb_state[SDL_SCANCODE_W]) player_input.y += 1;
+    if (keyb_state[SDL_SCANCODE_S]) player_input.y -= 1;
+    vec2_normalize(&player_input);
+
+    player_atk_input = (Vec2){0};
+    if (keyb_state[SDL_SCANCODE_RIGHT]) player_atk_input.x += 1;
+    if (keyb_state[SDL_SCANCODE_LEFT]) player_atk_input.x -= 1;
+    if (keyb_state[SDL_SCANCODE_UP]) player_atk_input.y += 1;
+    if (keyb_state[SDL_SCANCODE_DOWN]) player_atk_input.y -= 1;
+    vec2_normalize(&player_atk_input);
+
+    if (keyb_state[SDL_SCANCODE_SPACE] && !last_keyb_state[SDL_SCANCODE_SPACE]) {
+        player_dash_input = true;
+    }
+}
+
+void input_clean() {
+    player_dash_input = false;
+}
+
+void update() {
+    { // Player
+    player_move_dir = player_input;
+    if (player_move_dir.x != 0 || player_move_dir.y != 0)
+        player_move_dir_nozero = player_move_dir;
+
+    player_atk_able = time_s - player_atk_timest > player_atk_cd;
+    if (player_atk_able) {
+        if (player_atk_input.x != 0 || player_atk_input.y != 0) {
+            player_just_atk = true;
+            player_atking = true;
+
+            player_atk_timest = time_s;
+
+            if (fabs(player_atk_input.x) > 0)
+                player_atk_dir = player_atk_input.x > 0 ? 0 : 1;
+            else
+                player_atk_dir = player_atk_input.y > 0 ? 2 : 3;
+        }
+    }
+
+    if (time_s - player_atk_timest > player_atk_dur) player_atking = false;
+
+    player_dash_able = time_s - player_dash_timest > player_dash_cd;
+    if (player_dash_able) {
+        if (player_dash_input) {
+            player_just_dash = true;
+            player_dashing = true;
+
+            player_dash_timest = time_s;
+
+            player_dash_dir = player_move_dir_nozero;
+        }
+    }
+
+    if (time_s - player_dash_timest > player_dash_dur) player_dashing = false;
+
+    // =====================
+    Vec2 move_delta; vec2_scale(&move_delta, player_move_dir, player_move_speed * tick_delta_ms);
+
+    if (player_just_atk) { }
+    if (player_atking) { }
+
+    if (player_dashing) {
+        vec2_scale(&move_delta, player_dash_dir, player_dash_speed * tick_delta_ms);
+    }
+
+    vec2_add(&player_pos, player_pos, move_delta);
+
+    // =====================
+    player_just_atk = false;
+    }
+}
+
+void frame_update() {
+
+}
+
+void render_update() {
+    draw_sprite_wpos(player_sprite, player_pos, 0, (Vec2){.5, .5}, (Vec2){4, 4});
+}
 
 int main() {
     if (!SDL_Init(SDL_INIT_VIDEO)) { return 1; }
@@ -51,22 +182,7 @@ int main() {
                                      &window, &renderer)) { return 1; }
     SDL_GetWindowSizeInPixels(window, &window_w, &window_h);
 
-    arena_init(&global_ar, 100ull << 10 << 10); // 100mB
-
-    { // input
-    int numkeys;
-    SDL_GetKeyboardState(&numkeys);
-    last_keyb_state = arena_alloc(&global_ar, numkeys, 1);
-    }
-
-    texture_init(128);
-    u32 tex = texture_new_help("img_player");
-
-    sprite_init(1024);
-    SDL_FRect srect = {0, 0, 20, 20};
-    player_sprite = sprite_new(tex, &srect);
-
-    draw_init(1024);
+    setup();
 
     uint64_t last_time_ms = 0;
     uint64_t accml_time_ms = 0;
@@ -94,12 +210,7 @@ int main() {
         int numkeys;
         keyb_state = SDL_GetKeyboardState(&numkeys);
 
-        player_input = (Vec2){0};
-        if (keyb_state[SDL_SCANCODE_D]) player_input.x += 1;
-        if (keyb_state[SDL_SCANCODE_A]) player_input.x -= 1;
-        if (keyb_state[SDL_SCANCODE_W]) player_input.y += 1;
-        if (keyb_state[SDL_SCANCODE_S]) player_input.y -= 1;
-        vec2_normalize(&player_input);
+        input_update();
 
         memcpy(last_keyb_state, keyb_state, numkeys);
         }
@@ -109,6 +220,8 @@ int main() {
             tick_flag = true;
 
             update();
+
+            input_clean();
         }
         tick_alpha = (float)accml_time_ms / (float)tick_delta_ms;
 
@@ -135,17 +248,4 @@ END:
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-}
-
-void update() {
-    Vec2 move_delta = {0}; vec2_scale(&move_delta, player_input, tick_delta_ms);
-    vec2_add(&player_pos, player_pos, move_delta);
-}
-
-void frame_update() {
-
-}
-
-void render_update() {
-    draw_sprite_wpos(player_sprite, player_pos, 0, (Vec2){.5, .5}, (Vec2){4, 4});
 }
