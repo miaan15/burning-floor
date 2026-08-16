3c07364046c43dd2616da1b1b12f16adbd1701a6
miaan15 <miaanstarman@gmail.com>
2026-08-16 10:51:51 +0700

    half brake

--- cmake/vendors.cmake
+++ cmake/vendors.cmake
@@ -1,40 +1,43 @@
 set(VENDOR_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendor)
 
-execute_process(
-    COMMAND bash -c [[
-        mkdir -p vendor
-    ]]
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-)
-
 # SDL
-if(NOT IS_DIRECTORY ${VENDOR_DIR}/sdl/build)
+if(NOT IS_DIRECTORY "${VENDOR_DIR}/sdl/build")
     execute_process(
-        COMMAND bash -c [[
-            set -e
-            cmake -B build \
-                -DCMAKE_BUILD_TYPE=Release \
-                -DSDL_TESTS=OFF
-            cmake --build build --config Release
-        ]]
-        WORKING_DIRECTORY ${VENDOR_DIR}/sdl
-        RESULT_VARIABLE __RES
+        COMMAND ${CMAKE_COMMAND} -B build
+            -DCMAKE_BUILD_TYPE=Release
+            -DSDL_TESTS=OFF
+        WORKING_DIRECTORY "${VENDOR_DIR}/sdl"
+        RESULT_VARIABLE __CONFIG_RES
     )
-    if(NOT __RES EQUAL 0)
-        message(FATAL_ERROR "Failed build SDL")
-        file(REMOVE_RECURSE ${VENDOR_DIR}/sdl/build)
+
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} --build build --config Release
+        WORKING_DIRECTORY "${VENDOR_DIR}/sdl"
+        RESULT_VARIABLE __BUILD_RES
+    )
+
+    if(NOT __BUILD_RES EQUAL 0)
+        file(REMOVE_RECURSE "${VENDOR_DIR}/sdl/build")
+        message(FATAL_ERROR "Failed to build SDL")
     endif()
 
-    execute_process(
-        COMMAND bash -c [[
-            echo "*" > .gitignore
-        ]]
-        WORKING_DIRECTORY ${VENDOR_DIR}/sdl/build
-    )
+    file(WRITE "${VENDOR_DIR}/sdl/build/.gitignore" "*\n")
 endif()
 
 add_library(SDL SHARED IMPORTED)
-set_target_properties(SDL PROPERTIES
-    IMPORTED_LOCATION ${VENDOR_DIR}/sdl/build/libSDL3.so
-)
-target_include_directories(SDL INTERFACE ${VENDOR_DIR}/sdl/include)
+target_include_directories(SDL INTERFACE "${VENDOR_DIR}/sdl/include")
+
+if(WIN32)
+    set_target_properties(SDL PROPERTIES
+        IMPORTED_LOCATION "${VENDOR_DIR}/sdl/build/SDL3.dll"
+        IMPORTED_IMPLIB   "${VENDOR_DIR}/sdl/build/libSDL3.dll.a"
+    )
+elseif(APPLE)
+    set_target_properties(SDL PROPERTIES
+        IMPORTED_LOCATION "${VENDOR_DIR}/sdl/build/libSDL3.dylib"
+    )
+else()
+    set_target_properties(SDL PROPERTIES
+        IMPORTED_LOCATION "${VENDOR_DIR}/sdl/build/libSDL3.so"
+    )
+endif()
